abcfefb8a164f88afdcbb42a80f35ecfc6d1a18a
Kaitlyn Folsom <kaitlynfolsom@Kaitlyns-MBP.fios-router.home>
2017-10-12 15:58:34 -0400

    pushing to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,33 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "password",
+//   database: "burgers_db"
+// });
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+var connection;
+
+if(process.env.JAWSDB_URL){
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+	connection = mysql.createConnection({
+		host: "localhost",
+		root: "root",
+		password: "password",
+		database: "burgers_db"
+	});
+};
+
+connection.connect();
+
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
 module.exports = connection;
